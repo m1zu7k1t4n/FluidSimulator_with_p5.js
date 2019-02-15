e75d07d9ac9381ec9e1cb03364a5761423d26f2b
K.Arayama <sinzan062871@gmail.com>
2019-02-15 22:13:29 +0900

    first commit

---
+++ public/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+
+}
+
+function draw() {
+  background(0)
+}
+
+function mousePressed() {
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+}
